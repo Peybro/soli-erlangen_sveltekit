cf3a2d71814601ac9f7df1e7d6fecc7347fddfbc
Thomas Kretschmann <t.kretschmann@t-online.de>
2023-03-22 20:42:43 +0100

    added better set

--- src/routes/set/+page.server.ts
+++ src/routes/set/+page.server.ts
@@ -1,34 +1,71 @@
+import type { Actions, PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
+
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
-import db from '../../lib/services/firebase';
+import db from '$lib/services/firebase';
 
-export const load = async ({ depends }) => {
-	depends('app:bannerSettings');
+// See https://zod.dev/?id=primitives for schema syntax
+const schema = z.object({
+	enabled: z.boolean().default(false),
+	title: z.string().min(6).default('[Titel]'),
+	description: z.string().min(6).optional().default('[Beschreibung]]'),
+	bgColor: z.string().default('success'),
+	password: z.string()
+});
 
+export const load = (async (event) => {
 	const docRef = doc(db, 'banner', 'settings');
 	const docSnap = await getDoc(docRef);
 
-	if (docSnap.exists()) {
-		return {
-			enabled: docSnap.data().enabled,
-			title: docSnap.data().title,
-			description: docSnap.data().description,
-			bgColor: docSnap.data().bgColor
-		};
-	} else {
-		console.log('No such document!');
-	}
+	// if (docSnap.exists()) {
+	// 	schema.parse({
+	// 		enabled: docSnap.data().enabled,
+	// 		title: docSnap.data().title,
+	// 		description: docSnap.data().description,
+	// 		bgColor: docSnap.data().bgColor,
+	// 		password: ''
+	// 	});
+	// } else {
+	// 	console.log('No such document!');
+	// }
 
-	return;
-};
+	// Server API:
+	const form = await superValidate(
+		event,
+		docSnap.exists()
+			? z.object({
+					enabled: z.boolean().default(docSnap.data().enabled),
+					title: z.string().min(6).default(docSnap.data().title),
+					description: z.string().min(6).optional().default(docSnap.data().description),
+					bgColor: z.string().default(docSnap.data().bgColor),
+					password: z.string()
+			  })
+			: schema
+	);
+
+	// Always return { form } in load and form actions.
+	return { form };
+}) satisfies PageServerLoad;
 
 export const actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
-		// console.log(formData);
+	default: async (event) => {
+		// Same syntax as in the load function
+		const form = await superValidate(event, schema);
+		// console.log('POST', form);
 
+		// Convenient validation check:
+		if (!form.valid) {
+			// Again, always return { form } and things will just work.
+			return fail(400, { form });
+		}
+
+		// TODO: Do something with the validated data
 		const auth = getAuth();
-		const pw = formData.get('pw')?.toString() || '';
+		const pw = form.data.password;
+		let wrongPasswordError = false;
 
 		await signInWithEmailAndPassword(auth, 'vorstand@soli-erlangen.de', pw)
 			.then(async (userCredential) => {
@@ -36,17 +73,25 @@ export const actions = {
 				// const user = userCredential.user;
 				// console.log(user);
 				await setDoc(doc(db, 'banner', 'settings'), {
-					enabled: formData.get('enabled') || false,
-					title: formData.get('title') || '',
-					description: formData.get('description') || '',
-					bgColor: formData.get('bgColor') || ''
+					enabled: form.data.enabled,
+					title: form.data.title,
+					description: form.data.description,
+					bgColor: form.data.bgColor
 				});
 			})
 			.catch((error) => {
-				// const errorCode = error.code;
-				// const errorMessage = error.message;
-				// console.log(errorCode, errorMessage);
-				console.log('Falsches Passwort!');
+				wrongPasswordError = true;
+				// errors.code = error.code;
+				form.valid = false;
+				form.errors.password = [
+					// error.message
+					'Falsches Passwort!'
+				];
 			});
+
+		if (wrongPasswordError) return fail(403, { form });
+
+		// Yep, return { form } here too
+		return { form };
 	}
-};
+} satisfies Actions;
--- src/routes/set/+page.svelte
+++ src/routes/set/+page.svelte
@@ -1,78 +1,164 @@
-<script>
-	export let data;
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { superForm } from 'sveltekit-superforms/client';
 
-	let enabled = data.enabled;
-	let title = data.title;
-	let description = data.description;
-	let bgColor = data.bgColor;
+	export let data: PageData;
+
+	// Client API:
+	const { form, errors, constraints, enhance, delayed } = superForm(data.form);
 </script>
 
-<div class={`alert alert-${bgColor}`} role="alert">
-	<h4 class="alert-heading">{title}</h4>
-	{#each description.split('\n') as line, i}
-		<p class="mb-0">
-			{line}
-		</p>
-	{/each}
+<div class={`banner bg-${$form.bgColor}`}>
+	<h1>{$form.title}</h1>
+	<p>{$form.description}</p>
 </div>
 
-<h2 class="heading">Benachrichtigung bearbeiten</h2>
-<div class="border p-2">
-	<form method="post">
-		<div class="form-check form-switch float-end">
-			<input
-				class="form-check-input"
-				type="checkbox"
-				id="enabledCheckBox"
-				name="enabled"
-				checked={enabled}
-			/>
-			<label class="form-check-label" for="enabledCheckBox">Anzeigen?</label>
-		</div>
-		<div class="mb-3">
-			<label for="titleInput" class="form-label">Überschrift</label>
-			<input
-				id="titleInput"
-				type="text"
-				class="form-control"
-				placeholder="Titel"
-				name="title"
-				value={title}
-				on:input={(e) => (title = e.currentTarget.value)}
-			/>
-		</div>
-		<div class="mb-3">
-			<label for="descriptionInput" class="form-label">Beschreibung</label>
-			<textarea
-				id="descriptionInput"
-				class="form-control"
-				placeholder="Beschreibung"
-				name="description"
-				value={description}
-				on:input={(e) => (description = e.currentTarget.value)}
-			/>
-		</div>
-		<div class="mb-3">
-			<label for="bgColorSelect" class="form-label">Hintergrundfarbe</label>
-			<select
-				id="bgColorSelect"
-				class="form-select"
-				name="bgColor"
-				value={bgColor}
-				on:change={(e) => (bgColor = e.currentTarget.value)}
-			>
-				<option value="success">Grün</option>
-				<option value="warning">Gelb</option>
-				<option value="danger">Rot</option>
-				<option value="primary">Blau</option>
-			</select>
-		</div>
+<form method="POST" use:enhance>
+	<div class="form-checkbox">
+		<input
+			class=""
+			id="enabledCheckBox"
+			type="checkbox"
+			name="enabled"
+			bind:checked={$form.enabled}
+		/>
+		<label class="" for="enabledCheckBox">Anzeigen?</label>
+	</div>
 
-		<hr />
-		<div class="mb-3">
-			<label for="passwordInput" class="form-label">Passwort</label>
-			<input id="passwordInput" type="password" class="form-control" name="pw" />
-		</div>
-		<button class="btn btn-secondary" type="submit">Speichern</button>
-	</form>
-</div>
+	<div class="form-input">
+		<label for="titleInput" class="input-label">Überschrift</label>
+		<input
+			id="titleInput"
+			type="text"
+			class=""
+			placeholder="Titel"
+			name="title"
+			data-invalid={$errors.title}
+			bind:value={$form.title}
+			{...$constraints.title}
+		/>
+		<!-- {#if $errors.title}
+				<small class="invalid">{$errors.title}</small>
+			{/if} -->
+	</div>
+	<div class="form-input">
+		<label for="descriptionInput" class="">Beschreibung</label>
+		<textarea
+			id="descriptionInput"
+			class=""
+			placeholder="Beschreibung"
+			name="description"
+			rows="4"
+			bind:value={$form.description}
+		/>
+	</div>
+
+	<div class="form-input">
+		<label for="bgColorSelect" class="">Hintergrundfarbe</label>
+		<select id="bgColorSelect" class="" name="bgColor" bind:value={$form.bgColor}>
+			<option value="success">Grün</option>
+			<option value="warning">Gelb</option>
+			<option value="danger">Rot</option>
+			<option value="primary">Blau</option>
+		</select>
+	</div>
+
+	<div class="form-input">
+		<label for="passwordInput" class="">Passwort</label>
+		<input
+			id="passwordInput"
+			type="password"
+			class=""
+			name="password"
+			data-invalid={$errors.password}
+			bind:value={$form.password}
+			{...$constraints.password}
+		/>
+		{#if $errors.password}
+			<small class="invalid">{$errors.password}</small>
+		{/if}
+	</div>
+	<button class="" type="submit">Speichern</button>
+	{#if $delayed}Working...{/if}
+</form>
+
+<style lang="scss">
+	form {
+		margin: 1rem;
+		height: 100%;
+		background: #f1f1f1;
+		padding: 2rem;
+		border-radius: 10px;
+
+		.form-checkbox {
+			text-align: right;
+		}
+
+		.form-input {
+			display: flex;
+			flex-direction: column;
+			margin-bottom: 1rem;
+
+			input,
+			textarea,
+			select {
+				padding: 0.5rem;
+				border-radius: 5px;
+				border: 0.5px solid #ccc;
+				background-color: white;
+			}
+
+			label {
+			}
+		}
+
+		button[type='submit'] {
+			display: block;
+			width: 100%;
+			height: 50px;
+			border: none;
+			background-image: linear-gradient(120deg, #3498db, #8e44ad);
+			background-size: 200%;
+			color: #fff;
+			outline: none;
+			cursor: pointer;
+			transition: 0.5s;
+			border-radius: 10px;
+
+			&:hover {
+				background-position: right;
+			}
+		}
+
+		.invalid {
+			color: red;
+			margin-top: 0.3rem;
+		}
+	}
+
+	.banner {
+		padding: 1rem;
+		margin: 1rem;
+		border-radius: 0.5rem;
+
+		&.bg-success {
+			background-color: #d1e7dd;
+			color: #0f5152;
+		}
+
+		&.bg-warning {
+			background-color: #fff3cd;
+			color: #664d37;
+		}
+
+		&.bg-danger {
+			background-color: #f8d7da;
+			color: #842029;
+		}
+
+		&.bg-primary {
+			background-color: #cfe2ff;
+			color: #084298;
+		}
+	}
+</style>
