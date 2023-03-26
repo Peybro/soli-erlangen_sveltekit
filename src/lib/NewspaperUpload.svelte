a90ea3c1f978fd2e0be9687b4228e98d9d84bc07
Thomas Kretschmann <t.kretschmann@t-online.de>
2023-03-27 00:27:32 +0200

    newspaper upload (bad)

---
+++ src/lib/NewspaperUpload.svelte
@@ -0,0 +1,205 @@
+<script lang="ts">
+	import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+	import { ref, uploadBytes } from 'firebase/storage';
+	import { storage } from '$lib/services/firebase';
+
+	let newsPaperName = '';
+
+	let seasonFrom: number;
+	let seasonTo: number;
+	let yearFrom: number;
+	let yearTo: number;
+	let password = '';
+
+	let wrongPasswordError = false;
+	let wrongPasswordErrorMessage = '';
+
+	function handleSeasonFromChange() {
+		seasonTo = seasonFrom === 1 ? 2 : 1;
+		yearTo = seasonFrom === 2 ? yearFrom + 1 : yearFrom;
+	}
+
+	function handleSeasonToChange() {
+		seasonFrom = seasonTo === 1 ? 2 : 1;
+		yearTo = seasonFrom === 2 ? yearFrom + 1 : yearFrom;
+	}
+
+	function handleYearFromChange() {
+		yearTo = seasonFrom === 2 ? yearFrom + 1 : yearFrom;
+	}
+
+	function handleYearToChange() {
+		yearFrom = seasonFrom === 2 ? yearTo - 1 : yearTo;
+	}
+
+	$: yearTo = seasonFrom === 2 ? yearFrom + 1 : yearFrom;
+
+	$: {
+		const yearRegex = /20\d\d/;
+		const result = yearRegex.exec(newsPaperName);
+		if (newsPaperName !== undefined && result !== undefined && result !== null)
+			yearFrom = parseInt(result[0]);
+	}
+
+	$: {
+		const seasonRegex = /20\d\d_\d/;
+		const result = seasonRegex.exec(newsPaperName);
+		if (newsPaperName !== undefined && result !== undefined && result !== null)
+			seasonFrom = parseInt(result[0].split('_')[1]);
+	}
+
+	$: seasonTo = seasonFrom === 1 ? 2 : 1;
+
+	async function uploadPDF(event: any) {
+		event.preventDefault();
+		wrongPasswordError = true;
+
+		let file = event.target[0].files[0];
+
+		const uploadRef = ref(storage, `Vereinsblatt/soli_info_${yearFrom}_${seasonFrom}.pdf`);
+
+		const auth = getAuth();
+
+		await signInWithEmailAndPassword(auth, 'vorstand@soli-erlangen.de', password || '')
+			.then(async (userCredential) => {
+				//? Signed in
+				// const user = userCredential.user;
+				// console.log(user);
+
+				await uploadBytes(uploadRef, file)
+					.then((snapshot) => {
+						console.log('Uploaded a blob or file!');
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			})
+			.catch((error) => {
+				wrongPasswordError = true;
+				wrongPasswordErrorMessage = error.message;
+			});
+	}
+</script>
+
+<div class="card p-3">
+	<form enctype="multipart/form-data" on:submit={(e) => uploadPDF(e)}>
+		<label for="formFile" class="form-label">Neue Zeitung hochladen</label>
+		<input
+			class="form-control"
+			type="file"
+			accept=".pdf"
+			id="formFile"
+			name="path"
+			bind:value={newsPaperName}
+		/>
+
+		<div class="p-2 my-2 card">
+			<div class="row">
+				<div class="col-1">
+					<span class="from-to">Von</span>
+				</div>
+
+				<div class="col">
+					<label for="seasonFrom">Jahreszeit</label>
+					<select
+						class="form-select"
+						id="seasonFrom"
+						name="seasonFrom"
+						bind:value={seasonFrom}
+						on:change={handleSeasonFromChange}
+					>
+						<option value={0} disabled>auswählen</option>
+						<option value={1}>Frühling</option>
+						<option value={2}>Herbst</option>
+					</select>
+				</div>
+				<div class="col">
+					<label for="yearFrom">Jahr</label>
+					<input
+						class="form-control"
+						type="number"
+						inputmode="numeric"
+						min={new Date().getFullYear()}
+						id="yearFrom"
+						name="yearFrom"
+						bind:value={yearFrom}
+						on:change={handleYearFromChange}
+					/>
+				</div>
+			</div>
+		</div>
+
+		<div class="p-2 my-2 card">
+			<div class="row">
+                <div class="col-1">
+					<span class="from-to">Bis</span>
+				</div>
+
+				<div class="col">
+					<label for="seasonTo">Jahreszeit</label>
+					<select
+						class="form-select"
+						id="seasonTo"
+						name="seasonTo"
+						bind:value={seasonTo}
+						on:change={handleSeasonToChange}
+					>
+						<option value={0} disabled>auswählen</option>
+						<option value={1}>Frühling</option>
+						<option value={2}>Herbst</option>
+					</select>
+				</div>
+				<div class="col">
+					<label for="yearTo">Jahr</label>
+					<input
+						class="form-control"
+						type="number"
+						inputmode="numeric"
+						min={new Date().getFullYear()}
+						id="yearTo"
+						name="yearTo"
+						bind:value={yearTo}
+						on:change={handleYearToChange}
+					/>
+				</div>
+			</div>
+		</div>
+
+		<div class="form-input">
+			<label for="password" class="form-label">Passwort</label>
+			<input
+				class="form-control"
+				type="password"
+				id="password"
+				name="password"
+				placeholder="Passwort"
+				bind:value={password}
+			/>
+			{#if wrongPasswordError}
+				<small class="invalid">{wrongPasswordErrorMessage}</small>
+			{/if}
+		</div>
+
+		<button
+			type="submit"
+			class="btn btn-primary my-3"
+			disabled={!(
+				newsPaperName.length > 0 &&
+				password.length > 0 &&
+				yearTo !== undefined &&
+				yearFrom !== undefined &&
+				yearTo - yearFrom <= 1
+			)}>Hochladen</button
+		>
+	</form>
+</div>
+
+<style>
+    .from-to {
+        font-size: 1.5rem;
+    }
+
+	.invalid {
+		color: red;
+	}
+</style>
