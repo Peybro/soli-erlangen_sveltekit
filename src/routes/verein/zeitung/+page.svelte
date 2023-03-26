bb2249a4b6aa2699c84cf6fdf95264bcbb24fd0e
Thomas Kretschmann <t.kretschmann@t-online.de>
2023-03-26 13:05:44 +0200

    fixed /zeitung

--- src/routes/verein/zeitung/+page.svelte
+++ src/routes/verein/zeitung/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+	import { getDownloadURL } from 'firebase/storage';
 	export let data;
-
-	$: data.files.sort().reverse();
 </script>
 
 <h2 class="heading">Zeitung</h2>
 
 {#await data.files}
-	<h2>Lade PDF Dateien...</h2>
-{:then files}
-	{#if files.length > 0}
-		<div class="accordion">
-			<div class="accordion-item">
+	<div class="alert alert-info" role="alert">Lade PDF Dateien...</div>
+{:then res}
+	<div class="accordion">
+		<div class="accordion-item">
+			{#await getDownloadURL([...res.items].reverse()[0])}
+				<div class="alert alert-info" role="alert">Lade aktuelle Ausgabe...</div>
+			{:then url}
 				<h2 class="accordion-header" id="panelsStayOpen-headingOne">
 					<button
 						class="accordion-button"
@@ -21,64 +22,76 @@
 					>
 						Aktuelle Ausgabe:
 						<span class="fw-bold"
-							>{files[0].split('_')[3].startsWith('1.pdf')
+							>{url.split('%2F')[1].split('?')[0].split('.')[1].startsWith('1.pdf')
 								? 'Frühling-Herbst'
 								: 'Herbst ' +
-								  files[0].split('_')[2] +
+								  url.split('_')[2] +
 								  '-Frühling' +
 								  ' ' +
-								  (parseInt(files[0].split('_')[2]) + 1)}</span
+								  (parseInt(url.split('_')[2]) + 1)}</span
 						>
 					</button>
 				</h2>
 				<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
 					<div class="accordion-body">
-						<iframe src={files[0]} title={files[0]} height="600px" loading="lazy" />
+						<iframe
+							src={url}
+							title={url.split('%2F')[1].split('?')[0].split('.')[0].toString()}
+							height="600px"
+							loading="lazy"
+						/>
 					</div>
 				</div>
-			</div>
-			<div class="accordion-item">
-				<h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-					<button
-						class="accordion-button collapsed"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#panelsStayOpen-collapseTwo"
-					>
-						Ältere Ausgaben:
-					</button>
-				</h2>
-				<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-					<div class="accordion-body">
-						<div class="grid">
-							{#each files.slice(1) as pdf}
+			{/await}
+		</div>
+
+		<div class="accordion-item">
+			<h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+				<button
+					class="accordion-button collapsed"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#panelsStayOpen-collapseTwo"
+				>
+					Ältere Ausgaben:
+				</button>
+			</h2>
+			<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+				<div class="accordion-body">
+					<div class="grid">
+						{#each [...res.items].reverse() as itemRef}
+							{#await getDownloadURL(itemRef)}
+								<div class="alert alert-info" role="alert">Lade ältere Ausgaben...</div>
+							{:then url}
 								<div>
 									<h4>
-										{pdf.split('_')[3].startsWith('1.pdf')
-											? 'Frühling-Herbst ' + pdf.split('_')[2]
+										{url.split('_')[3].startsWith('1.pdf')
+											? 'Frühling-Herbst ' + url.split('_')[2]
 											: 'Herbst ' +
-											  pdf.split('_')[2] +
+											  url.split('_')[2] +
 											  '-Frühling' +
 											  ' ' +
-											  (parseInt(pdf.split('_')[2]) + 1)}
+											  (parseInt(url.split('_')[2]) + 1)}
 									</h4>
 									<iframe
-										src={pdf}
-										title={pdf.split('.')[0].toString()}
+										src={url}
+										title={url.split('%2F')[1].split('?')[0].split('.')[0].toString()}
 										height="400px"
 										loading="lazy"
 									/>
-									<a href={pdf}>Größer anzeigen</a>
+									<a href={url}>Größer anzeigen</a>
 								</div>
-							{/each}
-						</div>
+							{/await}
+						{/each}
 					</div>
 				</div>
 			</div>
 		</div>
-	{/if}
+	</div>
 {:catch error}
-	<p style="color: red">{error.message}</p>
+	<div class="alert alert-danger" role="alert">
+		{error.message}
+	</div>
 {/await}
 
 <style lang="scss">
--- src/routes/verein/zeitung/+page.ts
+++ src/routes/verein/zeitung/+page.ts
@@ -1,27 +1,6 @@
-import { ref, listAll, getDownloadURL } from 'firebase/storage';
+import { ref, listAll } from 'firebase/storage';
 import { storage } from '$lib/services/firebase';
 
 export const load = async () => {
-	const loadPDFs = async () => {
-		const files: string[] = [];
-
-		await listAll(ref(storage, 'Vereinsblatt'))
-			.then((res) => {
-				res.items.forEach((itemRef) => {
-					getDownloadURL(itemRef).then((url) => {
-						// console.log(url);
-						files.unshift(url);
-					});
-				});
-
-				// console.log(files);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-
-		return files;
-	};
-
-	return { files: loadPDFs() };
+	return { files: listAll(ref(storage, 'Vereinsblatt')) };
 };
