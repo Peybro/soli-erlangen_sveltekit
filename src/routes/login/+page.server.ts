56c4e20c728e8de67b033d33bfe066013444348d
Thomas Kretschmann <t.kretschmann@t-online.de>
2026-03-20 11:47:03 +0100

    fixed login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,54 +1,35 @@
-import {
-	browserSessionPersistence,
-	browserLocalPersistence,
-	setPersistence,
-	signInWithEmailAndPassword
-} from 'firebase/auth';
+import { browserLocalPersistence, setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '$lib/services/firebase';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {};
 
 export const actions = {
-	login: async ({ request, cookies, url }) => {
+	login: async ({ request, cookies }) => {
 		const data = await request.formData();
-
 		const email = data.get('email');
 		const password = data.get('password');
 
-		if (
-			email &&
-			email !== undefined &&
-			email !== '' &&
-			password &&
-			password !== undefined &&
-			password !== ''
-		) {
-			await setPersistence(auth, browserLocalPersistence);
-			return await signInWithEmailAndPassword(auth, email.toString(), password.toString())
-				.then(async (userCredential) => {
-					//? Signed in
-					const user = userCredential.user;
-					// console.log(user);
-					cookies.set('user', user.stsTokenManager.accessToken);
-					throw redirect(301, '/');
-				})
-				.catch((error) => {
-					// console.log(error);
-					if (error.status === 301) {
-						throw redirect(301, '/');
-					}
+		if (!email || !password) {
+			return { success: false, error: 'Bitte gib alle Felder an.' };
+		}
 
-					return {
-						success: false,
-						error: 'Falsches Passwort!'
-					};
-				});
-		} else {
-			return {
-				success: false,
-				error: 'Bitte gib alle Felder an.'
-			};
+		try {
+			await setPersistence(auth, browserLocalPersistence);
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email.toString(),
+				password.toString()
+			);
+
+			const token = await userCredential.user.getIdToken();
+			cookies.set('user', token, { path: '/' });
+
+			throw redirect(301, '/');
+		} catch (error: any) {
+			if (error.status === 301) throw error;
+
+			return { success: false, error: 'Falsches Passwort!' };
 		}
 	}
 };
